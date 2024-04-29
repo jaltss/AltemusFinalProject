95107ce70341700be72f4f83cf4c1e98417ef195
jaltss <jacksonaltemus10@gmail.com>
2024-04-29 18:35:33 -0400

    Final Project Initial Push

---
+++ AltemusFinalProject/Assets/SceneTransition.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string roomName;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit an object");
+        if (other.gameObject.CompareTag("Door"))
+        {
+            Debug.Log("hit a door");
+            SceneManager.LoadScene(roomName);
+        }
+    }
+}
